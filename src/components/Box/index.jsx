28da0c996d9d4fe31c6deec7addbf71a5f759dd0
Ritesh <sinharitesh919@gmail.com>
2023-11-23 12:55:38 +0530

    created the Box button component

---
+++ src/components/Box/index.jsx
@@ -0,0 +1,12 @@
+import "./index.css";
+
+const Box = (props) => {
+  const { value, onSquare } = props;
+  return (
+    <button className="box" onClick={onSquare}>
+      {value}
+    </button>
+  );
+};
+
+export default Box;
